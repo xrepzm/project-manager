468501fd5328918db46ec9a3ae3a9fddacb432ac
Nandor Dudas <x.rep.zm@gmail.com>
2016-12-25 21:08:22 +0100

    new issue

--- server.js
+++ server.js
@@ -1,74 +1,74 @@
-require('shelljs/global')
+require("shelljs/global")
 
-const WebSocketServer = require('ws').Server
+const WebSocketServer = require("ws").Server
 const wss = new WebSocketServer({ port: 8080 })
 
 // https://mostafa-samir.github.io/async-recursive-patterns-pt2/
 
-wss.on('connection', ws => {
-  ws.send(':connected:')
+wss.on("connection", ws => {
+  ws.send(":connected:")
 
-  ws.on('message', message => createProject(JSON.parse(message).projectname))
+  ws.on("message", message => createProject(JSON.parse(message).projectname))
 
   createProject = projectName => {
-    const rootuser = 'root'
-    const rootpass = ''
-    const param = require('change-case').paramCase(projectName)
-    const pascal = require('change-case').pascalCase(projectName)
-    const dbname = require('change-case').snakeCase(projectName)
+    const rootuser = "root"
+    const rootpass = ""
+    const param = require("change-case").paramCase(projectName)
+    const pascal = require("change-case").pascalCase(projectName)
+    const dbname = require("change-case").snakeCase(projectName)
     const dbuser = dbname
-    const dbpass = 'DBpasS2017&'
+    const dbpass = "DBpasS2017&"
 
     if (/^win/.test(process.platform)) {
-      require('dns').resolve('google.com', error => {
+      require("dns").resolve("google.com", error => {
         if (error) {
-          console.log('No connection')
+          console.log("No connection")
         } else {
 
-          cd('..')
-          ws.send('Creating directory...\n')
+          cd("..")
+          ws.send("Creating directory...\n")
 
           mkdir(param)
           cd(`${param}/`)
 
-          ws.send('Creating git flow...\n')
+          ws.send("Creating git flow...\n")
 
-          a = exec('git flow init -fd', { async: true })
-          a.stdout.on('data', data => ws.send(data))
-          a.on('close', code => {
+          a = exec("git flow init -fd", { async: true })
+          a.stdout.on("data", data => ws.send(data))
+          a.on("close", code => {
             if ( ! code) {
-              if (which('laraveL')) {
-                ws.send('Install Laravel...\n')
+              if (which("laraveL")) {
+                ws.send("Install Laravel...\n")
 
-                b = exec('laravel new', { async: true })
+                b = exec("laravel new", { async: true })
 
-                b.stdout.on('data', data => ws.send(data))
-                b.on('close', code => {
+                b.stdout.on("data", data => ws.send(data))
+                b.on("close", code => {
                   if ( ! code) {
-                    ws.send('Copying EditorConfig file...\n')
+                    ws.send("Copying EditorConfig file...\n")
 
                     cp(`${__dirname}\\.editorconfig`, `../${param}`)
 
-                    c = exec('echo *.lock >> .gitignore', { async: true })
-                    c.on('close', code => {
+                    c = exec("echo *.lock >> .gitignore", { async: true })
+                    c.on("close", code => {
                       if ( ! code) {
-                        d = exec('echo /bower_components >> .gitignore', { async: true })
-                        d.on('close', code => {
+                        d = exec("echo /bower_components >> .gitignore", { async: true })
+                        d.on("close", code => {
                           if ( ! code) {
-                            ws.send('Setup application name...\n')
+                            ws.send("Setup application name...\n")
 
                             e = exec(`php artisan app:name ${pascal}`, { async: true })
-                            e.stdout.on('data', data => ws.send(data))
-                            e.on('close', code => {
+                            e.stdout.on("data", data => ws.send(data))
+                            e.on("close", code => {
                               if ( ! code) {
-                                ws.send('Create symbolic link for uploads...\n')
-                                ws.send(exec('php artisan storage:link').stdout)
-                                ws.send('Setup applications environment variables...\n')
+                                ws.send("Create symbolic link for uploads...\n")
+                                ws.send(exec("php artisan storage:link").stdout)
+                                ws.send("Setup applications environment variables...\n")
 
-                                sed('-i', /(APP_URL=http:\/\/)localhost/, `\$1${param}.dev`,  '.env')
-                                sed('-i', /(DB_DATABASE=)homestead/, `\$1${dbname}`, '.env')
-                                sed('-i', /(DB_USERNAME=)homestead/, `\$1${dbuser}`, '.env')
-                                sed('-i', /(DB_PASSWORD=)secret/, `\$1${dbpass}`, '.env')
+                                sed("-i", /(APP_URL=http:\/\/)localhost/, `\$1${param}.dev`,  ".env")
+                                sed("-i", /(DB_DATABASE=)homestead/, `\$1${dbname}`, ".env")
+                                sed("-i", /(DB_USERNAME=)homestead/, `\$1${dbuser}`, ".env")
+                                sed("-i", /(DB_PASSWORD=)secret/, `\$1${dbpass}`, ".env")
                                 exec(`echo ##${projectName} > readme.md`)
 
                                 ws.send(`<a href="http:\/\/${param}.dev" target="_blank">${projectName}</a>`)
@@ -76,31 +76,31 @@ wss.on('connection', ws => {
                                 if (rootpass.trim()) {
                                   exec(`mysql -u${rootuser} -p${rootpass} -e "CREATE DATABASE \`${dbname}\` CHARACTER SET utf8 COLLATE utf8_general_ci;"`)
                                   exec(`mysql -u${rootuser} -p${rootpass} -e "CREATE DATABASE \`${dbname}_testing\` CHARACTER SET utf8 COLLATE utf8_general_ci;"`)
-                                  exec(`mysql -u${rootuser} -p${rootpass} -e "CREATE USER \`${dbuser}\`@\`localhost\` IDENTIFIED BY '${dbpass}';"`)
+                                  exec(`mysql -u${rootuser} -p${rootpass} -e "CREATE USER \`${dbuser}\`@\`localhost\` IDENTIFIED BY "${dbpass}";"`)
                                   exec(`mysql -u${rootuser} -p${rootpass} -e "GRANT ALL PRIVILEGES ON \`${dbname}\`.* TO \`${dbuser}\`@\`localhost\`;"`)
                                   exec(`mysql -u${rootuser} -p${rootpass} -e "GRANT ALL PRIVILEGES ON \`${dbname}_testing\`.* TO \`${dbuser}\`@\`localhost\`;"`)
                                   exec(`mysql -u${rootuser} -p${rootpass} -e "FLUSH PRIVILEGES;"`)
                                 } else {
                                   exec(`mysql -u${rootuser} -e "CREATE DATABASE \`${dbname}\` CHARACTER SET utf8 COLLATE utf8_general_ci;"`)
                                   exec(`mysql -u${rootuser} -e "CREATE DATABASE \`${dbname}_testing\` CHARACTER SET utf8 COLLATE utf8_general_ci;"`)
-                                  exec(`mysql -u${rootuser} -e "CREATE USER \`${dbuser}\`@\`localhost\` IDENTIFIED BY '${dbpass}';"`)
+                                  exec(`mysql -u${rootuser} -e "CREATE USER \`${dbuser}\`@\`localhost\` IDENTIFIED BY "${dbpass}";"`)
                                   exec(`mysql -u${rootuser} -e "GRANT ALL PRIVILEGES ON \`${dbname}\`.* TO \`${dbuser}\`@\`localhost\`;"`)
                                   exec(`mysql -u${rootuser} -e "GRANT ALL PRIVILEGES ON \`${dbname}_testing\`.* TO \`${dbuser}\`@\`localhost\`;"`)
                                   exec(`mysql -u${rootuser} -e "FLUSH PRIVILEGES;"`)
                                 }
 
-                                ws.send('\nCommiting repositiory...\n')
-                                exec('git add .')
-                                exec('git commit -m "Laravel installed"')
-                                ws.send('Done!\n')
+                                ws.send("\nCommiting repositiory...\n")
+                                exec("git add .")
+                                exec("git commit -m \"Laravel installed\"")
+                                ws.send("Done!\n")
 
-                                if (which('subl')) {
-                                  exec('subl .')
+                                if (which("subl")) {
+                                  exec("subl .")
                                 }
 
-                                ws.send('Reload laragon...\n')
+                                ws.send("Reload laragon...\n")
 
-                                setTimeout(() => exec('..\\..\\laragon reload'), 5000)
+                                setTimeout(() => exec("..\\..\\laragon reload"), 5000)
                               }
                             })
                           }
